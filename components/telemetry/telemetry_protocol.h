be01418d7f704d2648357b1bb06dffc71da0546a
Rowell Niño Dinolan <rdinolan@spinginemail.com>
2020-12-16 14:34:45 +0800

    remove typedef struct in telemetry_protocol.h

--- components/telemetry/telemetry_protocol.h
+++ components/telemetry/telemetry_protocol.h
@@ -3,7 +3,6 @@
 
 #include <stdint.h>
 
-
 enum {
     TELEMETRY_TYPE_AUTH = 0x01,
     TELEMETRY_TYPE_LOG  = 0x02,
@@ -14,8 +13,7 @@ enum {
     TELEMETRY_DEVICE_BSCAN = 0x02,
 };
 
-typedef struct
-{
+typedef struct {
     uint8_t type;
     uint8_t device_type;
     uint16_t reqNo;
@@ -24,18 +22,12 @@ typedef struct
     uint8_t instanceID[6];
 } __attribute__((packed)) telemetry_t;
 
-typedef struct
-{
+typedef struct {
     uint8_t type;
     uint8_t device_type;
     uint16_t reqNo;
     uint64_t serial;
     char uuid[36];
 } __attribute__((packed)) telemetry_simple_t;
-
-typedef struct
-{
-    
-}
 
 #endif /* COMPONENTS_TELEMETRY_TELEMETRY_PROTOCOL */
